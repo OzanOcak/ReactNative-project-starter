1f0caf17e6229fd24a4128cedcd86d3546f7ebe4
OzanOcak <ozanocak@gmail.com>
2023-06-21 23:03:17 +0300

    expo nac

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,51 +1,17 @@
-import { View, Text, Button } from "react-native";
-import React from "react";
-import { Stack, useRouter } from "expo-router";
+import { Stack } from "expo-router";
 
-const StackLayout = () => {
-  const router = useRouter();
+export default function HomeLayout() {
   return (
     <Stack
       screenOptions={{
-        headerStyle: { backgroundColor: "black" },
+        headerStyle: {
+          backgroundColor: "blue",
+        },
         headerTintColor: "white",
-        headerTitleStyle: { fontWeight: "bold" },
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
       }}
-    >
-      <Stack.Screen
-        name="index"
-        options={{ headerTitle: "Login", headerShown: false }}
-      ></Stack.Screen>
-      <Stack.Screen
-        name="register"
-        options={{
-          headerTitle: "Create account",
-          headerRight: () => (
-            <Button
-              title="Open"
-              onPress={() => {
-                router.push("modal"); // useRouter
-              }}
-            />
-          ),
-        }}
-      ></Stack.Screen>
-      <Stack.Screen
-        name="modal"
-        options={{
-          presentation: "modal",
-          headerRight: () => (
-            <Button
-              title="Close"
-              onPress={() => {
-                router.back(); // useRouter
-              }}
-            />
-          ),
-        }}
-      />
-    </Stack>
+    />
   );
-};
-
-export default StackLayout;
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,6 @@
-import { View, Text, Pressable } from "react-native";
-import React from "react";
-import { Link } from "expo-router";
+import { Redirect } from "expo-router";
 
-const LoginPage = () => {
-  const [isReady, setReady] = React.useState(false);
-
-  React.useEffect(() => {
-    // Perform some sort of async data or asset fetching.
-    setTimeout(() => {
-      setReady(true);
-    }, 1000);
-  }, []);
-  return (
-    <View className="flex justify-center items-center h-screen gap-2">
-      <Text>LoginPage</Text>
-      <Link href={"/register"} asChild>
-        <Pressable>
-          <Text>Create Account</Text>
-        </Pressable>
-      </Link>
-    </View>
-  );
+const Index = () => {
+  return <Redirect href="/cats" />;
 };
-
-export default LoginPage;
+export default Index;
